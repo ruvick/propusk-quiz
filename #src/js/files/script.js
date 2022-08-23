907312e67bba547fc268fa88de3261091343066a
Ruvick <rudikov.web@ya.ru>
2022-08-23 17:52:11 +0300

    Адаптив модалки

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -2,120 +2,91 @@
 // document.addEventListener('DOMContentLoaded', () => {
 // });
 
-const iconMenu = document.querySelector(".icon-menu");
-const body = document.querySelector("body");
-const menuBody = document.querySelector(".mob-menu");
-const menuListItemElems = document.querySelector(".mob-menu__list");
-const mobsearch = document.querySelector(".header__mob-search-btn");
-const headsearch = document.querySelector(".header__search-mob");
+let step = 1
+let stepCount = 3
+let popupDescp = document.querySelector(".popup__descp");
 
-//BURGER
-if (iconMenu) {
-  iconMenu.addEventListener("click", function () {
-    iconMenu.classList.toggle("active");
-    body.classList.toggle("_lock");
-    menuBody.classList.toggle("active");
-  });
+function toStep() {
+  let selected = document.querySelector(".q_blk.active");
+  selected.classList.remove("active")
+
+  let tosl = document.querySelector(".q_blk.step" + step);
+  tosl.classList.add("active")
+
+  // let as = document.querySelector(".q_steps .as");
+  // as.classList.add("active")
+
+  // document.querySelector(".q_steps .as").innerHTML = step;
 }
 
-// Закрытие моб меню при клике на якорную ссылку
-if (menuListItemElems) {
-  menuListItemElems.addEventListener("click", function () {
-    iconMenu.classList.toggle("active");
-    body.classList.toggle("_lock");
-    menuBody.classList.toggle("active");
-  });
-}
+function checkStep() {
+  if (step == 1) {
+    let list = document.getElementsByName('prop[]');
 
-// Строка поиска на мобилках 
-if (mobsearch) {
-  mobsearch.addEventListener("click", function () {
-    headsearch.classList.toggle("_active");
-  });
-}
-
-// Закрытие моб меню при клике вне области меню 
-window.addEventListener('click', e => { // при клике в любом месте окна браузера
-  const target = e.target // находим элемент, на котором был клик
-  if (!target.closest('.icon-menu') && !target.closest('.mob-menu') && !target.closest('.header__mob-search-btn') && !target.closest('.header__search-mob') && !target.closest('._popup-link') && !target.closest('.popup')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
-    iconMenu.classList.remove('active') // то закрываем окно навигации, удаляя активный класс
-    menuBody.classList.remove('active')
-    body.classList.remove('_lock')
-    headsearch.classList.remove('_active')
-  }
-})
-
-// Плавная прокрутка
-const smotScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
-
-smotScrollElems.forEach(link => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault()
-    console.log(event);
-
-    const id = link.getAttribute('href').substring(1)
-    console.log('id : ', id);
-
-    document.getElementById(id).scrollIntoView({
-      behavior: 'smooth'
-    });
-  })
-});
-
-
-// Полоса прокрутки в шапке
-const scrollProgress = document.getElementById('scroll-progress');
-const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-window.addEventListener('scroll', () => {
-  const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
-});
-
-
-// Ползунок выбора цены
-const priceEl = document.querySelector(".price");
-
-function changePrice(price) {
-  priceEl.innerText = price;
-  console.log(price);
-};
-
-
-// Подсказки
-tippy('._tippy', {
-  content: "Подсказка",
-});
-
-
-// Поочередное открытие нескольких блоков меню, табы, либо что то еще
-const BarIconElems = document.querySelectorAll('.sidebar__menu-open');
-const BarLinkIconElems = document.querySelectorAll('.sidebar__menu-icon');
-const BarSubMenuElems = document.querySelectorAll('.sidebar__submenu');
-
-BarIconElems.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
-
-    if (!btn.classList.contains('sidebar__menu-icon_active')) {
-
-      BarSubMenuElems.forEach((BarSubMenuElem) => {
-        BarSubMenuElem.classList.remove('active')
-      });
-      BarIconElems.forEach((BarIconElem) => {
-        BarIconElem.classList.remove('sidebar__menu-icon_active')
-      });
-      BarLinkIconElems.forEach((BarLinkIconElem) => {
-        BarLinkIconElem.classList.remove('sidebar__menu-icon_active')
-      });
-
-      BarSubMenuElems[index].classList.add('active')
-      BarLinkIconElems[index].classList.add('sidebar__menu-icon_active')
-      btn.classList.add('sidebar__menu-icon_active')
-    } else {
-      BarSubMenuElems[index].classList.remove('active')
-      BarLinkIconElems[index].classList.remove('sidebar__menu-icon_active')
-      btn.classList.remove('sidebar__menu-icon_active')
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].checked == true) return true;
     }
-  })
-})
+  }
 
+  if (step == 2) {
+    return true;
+  }
+
+  if (step == 3) {
+    // let phone = q_tel.value;
+    // if ((phone != "") && (phone.indexOf("_") < 0)) return true;
+    return true;
+  }
+
+  return false;
+}
+
+function stepUp() {
+  if (!checkStep()) {
+    alert("Заполние поля");
+    return;
+  }
+
+  if (step == stepCount)
+    return;
+
+  step++
+  toStep()
+
+  if (step == stepCount) {
+    q_next.style.display = "none"
+    // q_send.style.display = "block"
+    popupDescp.style.display = "none"
+  } else {
+    q_next.style.display = "flex"
+    // q_send.style.display = "none"
+  }
+}
+
+function stepDown() {
+  if (step == 1) return;
+  step--
+  toStep()
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".q_steps .all");//.innerHTML = stepCount;
+
+  q_next.onclick = (e) => {
+    e.preventDefault();
+    stepUp();
+  }
+
+  q_prev.onclick = (e) => {
+    e.preventDefault();
+    stepDown();
+  }
+
+  // q_send.onclick = (e) => {
+  //   e.preventDefault();
+  //   if (!checkStep()) {
+  //     alert("Заполние поле 'Телефон'");
+  //     return;
+  //   }
+  // }
+});
